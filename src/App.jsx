85085dd99eccbe51ecadff092cbf85dafc4483c5
hjbarangan <hjbarangan@newbrighton.edu.ph>
2023-11-30 19:15:27 +0800

    fix: Redesign Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,22 @@
 // import { useState } from "react";
 
-import imageSrc from "./assets/images/default.png";
-import hungerGames from "./assets/images/hunger.jpg"
-import spiderVerse from "./assets/images/spiderverse.jpeg"
-import oppen from "./assets/images/oppen.jpg"
-import barbie from "./assets/images/barbie.jpg"
-
+import { data } from "./data";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <header>
-        <form id="form">
-          <input
-            type="text"
-            id="search"
-            placeholder="Search"
-            className="search"
-          />
-        </form>
-      </header>
       <main id="main">
-        {/* <div className="text-heading">
-          <h1>Lorem Ipsum</h1>
-        </div> */}
         <div className="movie-container">
-          <div className="movie">
-            <img src={hungerGames} alt="images"></img>
-          </div>
-
-          <div className="movie">
-            <img src={spiderVerse} alt="images"></img>
-          </div>
-
-          <div className="movie">
-            <img src={barbie} alt="images"></img>
-          </div>
-
-          <div className="movie">
-            <img src={oppen} alt="images"></img>
-          </div>
-
-          <div className="movie">
-            <img src={imageSrc} alt="images"></img>
-          </div>
-
-          <div className="movie">
-            <img src={imageSrc} alt="images"></img>
-          </div>
-
-          <div className="movie">
-            <img src={imageSrc} alt="images"></img>
-          </div>
-
-          <div className="movie">
-            <img src={imageSrc} alt="images"></img>
-          </div>
+          {data.map((item) => {
+            return (
+              <>
+                <div className="movie">
+                  <img src={item.imageSrc} alt="images"></img>
+                </div>
+              </>
+            );
+          })}
         </div>
       </main>
     </>
---
+++ src/data.js
@@ -0,0 +1,36 @@
+import barbie from './assets/images/barbie.jpg'
+import oppenheimer from './assets/images/oppen.jpg'
+import spidey from './assets/images/spiderverse.jpeg'
+import hunger from './assets/images/hunger.jpg'
+
+export const data = [
+    {
+      "id": 1,
+      "title": "Hello World",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at nunc eget odio aliquam consectetur. Integer id tortor sed nisl ullamcorper luctus.",
+      "release_year": "2020",
+      "imageSrc": barbie
+    },
+    {
+      "id": 1,
+      "title": "Hello World",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at nunc eget odio aliquam consectetur. Integer id tortor sed nisl ullamcorper luctus.",
+      "release_year": "2020",
+      "imageSrc": oppenheimer
+    },
+    {
+        "id": 1,
+        "title": "Hello World",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at nunc eget odio aliquam consectetur. Integer id tortor sed nisl ullamcorper luctus.",
+        "release_year": "2020",
+        "imageSrc": spidey
+      },
+      {
+        "id": 1,
+        "title": "Hello World",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at nunc eget odio aliquam consectetur. Integer id tortor sed nisl ullamcorper luctus.",
+        "release_year": "2020",
+        "imageSrc": hunger
+      }
+  ]
+  
