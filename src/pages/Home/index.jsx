b6c69e9817f90144483280b1e99fe3a25f9c0768
hjbarangan <hjbarangan@newbrighton.edu.ph>
2023-12-06 18:35:23 +0800

    fix: Fixed spacing and divs

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,32 +5,36 @@ import { getMovies } from "../../features/movies/slice";
 export default function Homepage() {
   const dispatch = useDispatch();
   const { data } = useSelector((state) => state.movie);
-  console.log("🚀 ~ file: index.jsx:8 ~ Home ~ data:", data);
 
   useEffect(() => {
     dispatch(getMovies());
   }, [dispatch]);
 
-  <main>
-    <div className="flex w-screen h-screen bg-red-300 justify-center items-center flex-wrap">
-      <div className="flex m-2 p-4 justify-center items-center object-center w-3/4 h-1/2 bg-black">
-        {data.map((item) => {
-          console.log("🚀 ~ file: index.jsx:18 ~ {data.map ~ item:", item);
-          return (
-            <div
-              className="flex bg-purple-300 h-full justify-center items-center relative overflow-hidden m-2"
-              key={item.id}
-            >
-              <h3>{item.title}</h3>
-              <img
-                src={item.imageSrc}
-                alt="image"
-                className="flex object-cover w-full h-full"
-              />
-            </div>
-          );
-        })}
+  return (
+    <main>
+      <div className="flex w-screen h-screen justify-center items-center flex-wrap">
+        <div className="flex m-2 p-4 justify-center items-center object-center h-1/2 ">
+          {data.map((item) => {
+            return (
+              <div
+                className="flex flex-col w-2/3  h-full justify-center items-center overflow-hidden m-2"
+                key={item.id}
+              >
+                <div className="flex flex-col w-full h-96 justify-center items-center">
+                  <img
+                    src={item.imageSrc}
+                    alt={item.title}
+                    className="object-cover h-full"
+                  />
+                  <div className="w-full p-3 flex flex-wrap justify-center">
+                    <p className="text-center text-sm font-bold text-white tracking-wide uppercase">{item.title}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  </main>;
+    </main>
+  );
 }
